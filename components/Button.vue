c54b4077b71d83b180de51058ef4e18fb9027999
daryaWork <daraselanina89009@gmail.com>
2025-08-28 10:03:15 +0300

    add routing

---
+++ components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.button {
+  background-color: var(--color-black);
+  color: var(--color-white);
+  padding: 0.5rem 2rem;
+  border-radius: 20px;
+  transition: opacity 0.3s ease;
+
+  &:hover {
+    opacity: 0.8;
+    transition: opacity 0.3s ease;
+  }
+}
+</style>
